b1a08affda6e55d648cfc2ab992c469cae3e739a
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-11-11 02:15:27 -0800

    Trying to integrate setup.py with pytest

--- setup.py
+++ setup.py
@@ -1,4 +1,25 @@
 from distutils.core import setup
+import sys
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name='World Engine',
@@ -18,6 +39,8 @@ setup(
               'engine.dynamics.rigid_body', 'engine.dynamics.quaternions', 'engine.feedback', 'engine.controllers',
               'engine.controllers.pid'],
     package_dir={'': 'world_engine'},
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest},
     url='',
     license='MIT',
     author='Ryan A. Rodriguez',
