1690c0bdccdaa86fccfe6fa3fafb4d0ea27cd31e
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-11-11 01:50:32 -0800

    Fixed stray while loop sitting in this test.

--- tests/message_passing_tests/plugin_server_test.py
+++ tests/message_passing_tests/plugin_server_test.py
@@ -7,7 +7,8 @@ socket = context.socket(zmq.REP)
 socket.bind("tcp://127.0.0.1:5555")
 
 # x, y, z, pitch, yaw, roll
-while True:
-    msg = socket.recv()
-    print("Got", msgpack.unpackb(msg))
-    socket.send(msg)
+if __name__ == '__main__':
+    while True:
+        msg = socket.recv()
+        print("Got", msgpack.unpackb(msg))
+        socket.send(msg)
