79b1d2bcc97deee454bf810e390951490f687de0
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-11-17 01:49:22 -0800

    New test directory

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+from setuptools.command.test import test as TestCommand
+
 try:
     from setuptools import setup
 except ImportError:
@@ -12,13 +15,34 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read().replace('.. :changelog:', '')
 
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 requirements = [
     # TODO: put package requirements here
 ]
 
 test_requirements = [
-    # TODO: put package test requirements here
+    'pytest'
 ]
+
+cmdclass = {'test': PyTest}
 
 setup(
     name='tower',
