da0d45d92b184b2b4c093308f3637eb65a1caba4
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-11-17 01:44:17 -0800

    New test directory

---
+++ tests/test_tower.py
@@ -0,0 +1,19 @@
+"""
+Tests for Tower module.
+"""
+import pytest
+from tower import tower
+
+
+class TestTower:
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    def test_something(self):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
