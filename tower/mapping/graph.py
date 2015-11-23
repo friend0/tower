852f57a1b023ec2015a7bb76f3b84c43c5c0a47d
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-11-22 22:06:44 -0800

    Try this; fix to graph for 2/3 compatibility

--- tower/mapping/graph.py
+++ tower/mapping/graph.py
@@ -3,6 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from future.utils import iteritems
+from future.utils import viewitems
 
 from collections import OrderedDict
 
@@ -73,7 +74,7 @@ class Graph(dict):
         self.num_vertices = 0
 
         # take graph input as dictionary and and add to dictionary
-        for vertex, neighbors in iteritems(OrderedDict(iteritems(self))):
+        for vertex, neighbors in viewitems(OrderedDict(viewitems(self))):
             self[vertex] = neighbors
             for neighbor, weight in neighbors.iteritems():
                 self[vertex][neighbor] = weight
