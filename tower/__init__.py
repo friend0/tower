ea44ed63d7134ed26351b7f447620dc40cba8447
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-12-12 19:47:10 -0800

    updated import of Tower class

--- tower/__init__.py
+++ tower/__init__.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from swarm.controller_plugins.quadrotor_plugins.quadrotor_pid_plugin import QuadrotorPID
+from tower.tower import Tower
+from tower.swarm.vehicles.crazyflie import Crazyflie
+from workflow_manager import WorkflowManager
+from map.graph import Graph
+
 __author__ = 'Ryan A. Rodriguez'
 __email__ = 'ryarodri@ucsc.edu'
 __version__ = '0.1.0'
 
-from tower import Tower
-from workflow_manager import WorkflowManager
+
