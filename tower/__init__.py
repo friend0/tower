49cb0e21e9647c6645303082fdd6e56951722203
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-12-13 00:23:22 -0800

    Added future imports to __init__

--- tower/__init__.py
+++ tower/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import (absolute_import, division, print_function, unicode_literals)
 from tower.swarm.controller_plugins.quadrotor_plugins.quadrotor_pid_plugin import QuadrotorPID
 from tower.tower import Tower
 from tower.swarm.vehicles.crazyflie import Crazyflie
