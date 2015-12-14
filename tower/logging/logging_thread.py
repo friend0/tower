16f10fed8de263171b874b9ea5388055aaeee622
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2015-12-13 20:43:03 -0800

    Changed from zmq SUB to PULL

--- tower/logging/logging_thread.py
+++ tower/logging/logging_thread.py
@@ -10,9 +10,14 @@ import structlog
 from logging import handlers
 
 
+class LoggingConfig(object):
+    def __init__(self):
+        pass
+
+
 class LogThread(threading.Thread):
 
-    def __init__(self, worker_port=None, test_dir=None):
+    def __init__(self, worker_port=None, log_directory=None):
         threading.Thread.__init__(self)
 
         structlog.configure(
@@ -35,20 +40,19 @@ class LogThread(threading.Thread):
         self.logger = structlog.getLogger()
         self.logger.setLevel(logging.DEBUG)
         # create file handler which logs messages down to the debug level to a file for postprocessing
-        if test_dir is None:
+        if log_directory is None:
             latest_experiment = logging.FileHandler('../logs/octrl.log', mode='w')
         else:
-            print(test_dir)
-            latest_experiment = logging.FileHandler(test_dir+'/octrl.log', mode='w')
+            latest_experiment = logging.FileHandler(log_directory+'/octrl.log', mode='w')
 
         latest_experiment.setLevel(logging.DEBUG)
         # create console handler with a higher log level
         console = logging.StreamHandler(stream=sys.stderr)
         console.setLevel(logging.WARNING)
-        if test_dir is None:
+        if log_directory is None:
             rotating = handlers.RotatingFileHandler('../logs/experiment_history.log', mode='w', maxBytes=128e+6, backupCount=5, delay=True)
         else:
-            rotating = handlers.RotatingFileHandler(test_dir+'/experiment_history.log', mode='w', maxBytes=128e+6, backupCount=5, delay=True)
+            rotating = handlers.RotatingFileHandler(log_directory+'/experiment_history.log', mode='w', maxBytes=128e+6, backupCount=5, delay=True)
 
         rotating.doRollover()
         rotating.setLevel(logging.DEBUG)
@@ -63,14 +67,14 @@ class LogThread(threading.Thread):
 
         if worker_port is None:
             worker_port = 5555 + 128
+        self.logger.info('Logging thread started...')
         self.logger.info('Logging initialized in {} on port {}'.format(self.name, worker_port))
 
-        context = zmq.Context.instance()
-        self.socket = context.socket(zmq.SUB)
-        self.socket.setsockopt(zmq.SUBSCRIBE, "")
+        context = zmq.Context()
 
-        worker_ip = 'tcp://localhost:{}'
-        self.socket.connect(worker_ip.format(worker_port))
+        self.socket = context.socket(zmq.PULL)
+        self.socket.bind("tcp://127.0.0.1:{}".format(worker_port))
+        self.logger.info('ZMQ subscriber socket initialized: ready to receive external log messages...')
 
     def run(self):
         levels = {'info': self.logger.info, 'debug': self.logger.debug, 'warning': self.logger.warning,
